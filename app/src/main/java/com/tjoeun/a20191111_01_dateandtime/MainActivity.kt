2f53332f6ba3255dea658b7bed936834e485b5c5
kj_cho <kj_cho@idealidea.co.kr>
2019-11-11 20:04:14 +0900

    날짜 선택 이벤트 처리

--- app/src/main/java/com/tjoeun/a20191111_01_dateandtime/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191111_01_dateandtime/MainActivity.kt
@@ -4,11 +4,13 @@ import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : BaseActivity() {
 
 //    시작 일자 / 시간을 모두 저장하고 있는 캘린더 변수.
+//    변수가 만들어지는 현재 일시가 저장되게 됨.
     var startDateTimeCalenar = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +25,16 @@ class MainActivity : BaseActivity() {
 
             var datePickerDialog = DatePickerDialog(this,
                 DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+
+//                    시작일시 변수에 선택값 반영.
+                    startDateTimeCalenar.set(year, month, dayOfMonth)
+
+//                    버튼에 2019-09-08 양식으로 출력.
+                    var sdf = SimpleDateFormat("yyyy-MM-dd (EEE)")
+
+//                    버튼에 sdf를 이용해서 선택된 날짜를 String으로 변환
+                    startDateBtn.text = sdf.format(startDateTimeCalenar.time)
+
 
                 },
                 startDateTimeCalenar.get(Calendar.YEAR),
