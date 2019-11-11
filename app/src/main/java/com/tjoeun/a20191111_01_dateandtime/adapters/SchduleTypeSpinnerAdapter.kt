ab828ef601a7f96ac0e9b112f0a3647e437e2b79
kj_cho <kj_cho@idealidea.co.kr>
2019-11-11 21:19:33 +0900

    드랍다운 / 일반겟뷰의 XML 분리

--- app/src/main/java/com/tjoeun/a20191111_01_dateandtime/adapters/SchduleTypeSpinnerAdapter.kt
+++ app/src/main/java/com/tjoeun/a20191111_01_dateandtime/adapters/SchduleTypeSpinnerAdapter.kt
@@ -21,7 +21,7 @@ class SchduleTypeSpinnerAdapter(context:Context, res:Int, list:ArrayList<Schedul
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var tempRow = convertView
         if (tempRow == null) {
-            tempRow = inf.inflate(R.layout.schedule_type_list_item, null)
+            tempRow = inf.inflate(R.layout.schedule_type_view_item, null)
         }
 
         var row = tempRow!!
@@ -30,7 +30,17 @@ class SchduleTypeSpinnerAdapter(context:Context, res:Int, list:ArrayList<Schedul
         return  row
     }
 
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.schedule_type_list_item, null)
+        }
 
+        var row = tempRow!!
+
+
+        return  row
+    }
 
 
 
