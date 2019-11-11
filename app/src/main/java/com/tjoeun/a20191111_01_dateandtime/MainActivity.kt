d1f07b1d0fe15a375a61ba1ed0625e0439afce8b
kj_cho <kj_cho@idealidea.co.kr>
2019-11-11 19:51:51 +0900

    일자 입력 이벤트 구현 (버튼 클릭)  - 날짜 선택 이벤트 미구현

--- app/src/main/java/com/tjoeun/a20191111_01_dateandtime/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191111_01_dateandtime/MainActivity.kt
@@ -1,17 +1,37 @@
 package com.tjoeun.a20191111_01_dateandtime
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : BaseActivity() {
+
+//    시작 일자 / 시간을 모두 저장하고 있는 캘린더 변수.
+    var startDateTimeCalenar = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
 
     override fun setupEvents() {
+        startDateBtn.setOnClickListener {
 
+            var datePickerDialog = DatePickerDialog(this,
+                DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+
+                },
+                startDateTimeCalenar.get(Calendar.YEAR),
+                startDateTimeCalenar.get(Calendar.MONTH),
+                startDateTimeCalenar.get(Calendar.DAY_OF_MONTH))
+
+            datePickerDialog.show()
+
+        }
     }
 
     override fun setValues() {
